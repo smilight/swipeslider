a7292a45c68d7897161ee399e1a9cabb1bbd3029
smilight <shensi@ukr.net>
2014-06-10 11:59:42 +0300

    rebase

--- js/main.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    $('.slider li').height($(window).height());
-    $('.slider').swipeslider({
-        nextBtnText: '<i class="glyphicon glyphicon-forward"/>',
-        prevBtnText: '<i class="glyphicon glyphicon-backward"/>',
-        connNextText: '<i class="glyphicon glyphicon-forward"/>',
-        connPrevText: '<i class="glyphicon glyphicon-backward"/>'
-    });
-})();
