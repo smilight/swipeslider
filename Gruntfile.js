ce30fb2203dd7e4e57137a6cb54735cf130ae147
smilight <shensi@ukr.net>
2014-06-10 12:07:10 +0300

    rebase

--- Gruntfile.js
+++ Gruntfile.js
@@ -97,10 +97,7 @@ module.exports = function (grunt) {
         modernizr: {
 
             dist: {
-                // [REQUIRED] Path to the build you're using for development.
                 "devFile": "src/components/modernizr/modernizr.js",
-
-                // [REQUIRED] Path to save out the built file.
                 "outputFile": "dist/js/vendor/modernizr.js",
 
                 // Based on default settings on http://modernizr.com/download/
@@ -108,44 +105,22 @@ module.exports = function (grunt) {
                     "shiv": true,
                     "printshiv": false,
                     "load": true,
-                    "mq": false,
+//                    "mq": false,
                     "cssclasses": true
                 },
 
-                // Based on default settings on http://modernizr.com/download/
-                "extensibility": {
-                    "addtest": false,
-                    "prefixed": false,
-                    "teststyles": false,
-                    "testprops": false,
-                    "testallprops": false,
-                    "hasevents": false,
-                    "prefixes": false,
-                    "domprefixes": false
-                },
-
-                // By default, source is uglified before saving
-                "uglify": true,
-
-                // Define any tests you want to implicitly include.
-                "tests": [],
-
-                // By default, this task will crawl your project for references to Modernizr tests.
-                // Set to false to disable.
-                "parseFiles": true,
-
-                // When parseFiles = true, this task will crawl all *.js, *.css, *.scss files, except files that are in node_modules/.
-                // You can override this by defining a "files" array below.
-                // "files" : {
-                // "src": []
-                // },
-
-                // When parseFiles = true, matchCommunityTests = true will attempt to
-                // match user-contributed tests.
-                "matchCommunityTests": false,
-
-                // Have custom Modernizr tests? Add paths to their location here.
-                "customTests": []
+//                "extensibility": {
+//                    "addtest": false,
+//                    "prefixed": false,
+//                    "teststyles": false,
+//                    "testprops": false,
+//                    "testallprops": false,
+//                    "hasevents": false,
+//                    "prefixes": false,
+//                    "domprefixes": false
+//                },
+//
+                "uglify": true
             }
 
         },
